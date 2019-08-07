34debce41ec237de4f4690f8feb687aeafe1ee84
Vishva Patel <43411093+VishvaP@users.noreply.github.com>
2019-08-07 12:21:05 +0530

    Add files via upload
    flex sensor code - updated

---
+++ Flex_Sensor_Motor.ino
@@ -0,0 +1,29 @@
+#include <Servo.h>
+
+const int pinflex = 0;
+
+Servo myServo;
+
+void setup() {
+
+myServo.attach(10);
+Serial.begin(9600);
+}
+
+void loop() {
+
+int flexpos;
+
+int servopos;
+
+flexpos = analogRead(pinflex);
+
+servopos = map(flexpos, 840, 900, 0, 180);
+
+servopos = constrain(servopos, 0, 180);
+
+myServo.write(servopos);
+//Serial.println(flexpos);
+delay(25);
+
+}
